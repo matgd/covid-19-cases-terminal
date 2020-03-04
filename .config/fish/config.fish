2a28e06dd8f58ce9afb34eea11aadb054ffcf6fc
matgd <matgd@users.noreply.github.com>
2020-03-04 23:57:45 +0100

    Added Poland cases.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,4 @@
 bash -c '(printf "\nCoronavirus cases: \e[1;91m%\'d \e[1;0m\n" $(~/get_covid19_cases.sh))'
+bash -c '(printf "Confirmed in Poland: \e[0;91m%\'d \e[1;0m\n" $(~/get_covid19_cases_poland_confirmed.sh))'
+bash -c '(printf "Deceased in Poland: \e[0;91m%\'d \e[1;0m\n" $(~/get_covid19_cases_poland_deceased.sh))'
+bash -c '(printf "Recovered in Poland: \e[0;32m%\'d \e[1;0m\n\n" $(~/get_covid19_cases_poland_recovered.sh))'
---
+++ get_covid19_cases_poland_confirmed.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl --max-time 3 -s 'https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/query?f=json&where=(Confirmed%20%3E%200)%20AND%20(Country_Region%3D%27Poland%27)&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Confirmed%20desc%2CCountry_Region%20asc%2CProvince_State%20asc&outSR=102100&resultOffset=0&resultRecordCount=250&cacheHint=true' | python3 -c "import sys, json; print(json.load(sys.stdin)['features'][0]['attributes']['Confirmed'])" 2> /dev/null || echo -1
