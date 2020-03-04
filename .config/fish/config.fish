a799c04670a3011cd6dc443c3d01e7bc6b4f5459
matgd <matgd@users.noreply.github.com>
2020-03-04 21:43:30 +0100

    Added fish support

---
+++ .config/fish/config.fish
@@ -0,0 +1,1 @@
+bash -c '(printf "\nCoronavirus cases: \e[1;91m%\'d \e[1;0m\n" $(~/get_covid19_cases.sh))'
---
+++ get_covid19_cases.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl --max-time 3 -s 'https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/query?f=json&where=Confirmed%20%3E%200&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Confirmed%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&outSR=102100&cacheHint=true' | python3 -c "import sys, json; print(json.load(sys.stdin)['features'][0]['attributes']['value'])" 2> /dev/null || echo -1
